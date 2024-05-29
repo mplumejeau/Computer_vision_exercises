dacdd8aa57b271e566fb189ac72771965b5e7733
maxime plumejeau <maxime.plumejeau@gmail.com>
2024-05-29 12:58:34 +0900

    factorize code into functions

--- Exercise_geometric_transform/Scripts/orb.py
+++ Exercise_geometric_transform/Scripts/orb.py
@@ -2,84 +2,126 @@ import numpy as np
 import cv2 as cv
 from scipy.linalg import solve
 
+# read 2 input images and convert them into grey scale images
+def read_and_grey_images(img1_path, img2_path):
+    img1 = cv.imread(img1_path)
+    img2 = cv.imread(img2_path)
+    gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+    gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+    return gray1, gray2
+
+# find keypoints of 2 input grey scale images, perform matches between both images and return the strongest matches into an array
+def match_keypoints(gray1, gray2, ratio):
+
+    # initiate ORB detector
+    orb = cv.ORB_create()
+
+    # kp --> Keypoints : array containing the feature points of the grayscale image
+    # des --> Descriptor : array of size [nb keypoints]x128 containing informations about feature points
+
+    # find the keypoints and compute the descriptors with ORB
+    kp1 = orb.detect(gray1, None)
+    kp2 = orb.detect(gray2, None)
+    kp1, des1 = orb.compute(gray1, kp1)
+    kp2, des2 = orb.compute(gray2, kp2)
+
+    print('nb keypoints img1 : ' + str(len(kp1)))
+
+    # draw keypoints and save the result image
+    img1_kp = gray1
+    img1_kp = cv.drawKeypoints(gray1, kp1, img1_kp)
+    cv.imwrite(res_path + 'orb_keypoints.jpg', img1_kp)
+
+    # match keypoints between both images using brute-force
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    # apply ratio test to select only the strongest matches
+    good_matches = []
+    for m,n in matches:
+        if m.distance < ratio * n.distance:
+            good_matches.append([m])
+
+    # draw matches and save the result image
+    img_matches = cv.drawMatchesKnn(gray1, kp1, gray2, kp2, good_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    cv.imwrite(res_path + 'orb_matches.jpg', img_matches)
+
+    # construct an array of pair of matched points
+    matched_points = []
+    for m in good_matches:
+        pt1 = kp1[m[0].queryIdx].pt
+        pt2 = kp2[m[0].trainIdx].pt
+        matched_points.append([pt1, pt2])
+
+    return matched_points
+
+# compute the matrix A and the vector b from the matched points between 2 images and the Jacobian matrix
+def compute_A_b(matched_points, J):
+
+    Jt = J.transpose()
+
+    A = [[0, 0], 
+         [0, 0]]
+    
+    b = [[0],
+         [0]]
+    
+    for m in matched_points:
+        # delta = [[x0 - x1], [y0 - y1]]
+        delta = [[m[0][0] - m[1][0]], [m[0][1] - m[1][1]]]
+        A = A + Jt.dot(J)
+        b = b + Jt.dot(delta)
+
+    return A, b
+
+# compute the translation parameters between 2 images using the matched keypoints and return the 3x3 transformation matrix 
+def compute_translation_matrix(matched_points):
+
+    # translation Jacobian matrix
+    J = np.array([[1, 0], 
+                  [0, 1]])
+    
+    # compute matrix A and vector b
+    A, b = compute_A_b(matched_points, J)
+
+    # compute translation vector p 
+    p = solve(A, b)
+    print(p)
+
+    # 3x3 transformation matrix
+    M = np.array([[1, 0, p[0][0]],
+                  [0, 1, p[1][0]],
+                  [0, 0, 0      ]])
+    
+    return M
+
+
+####### main #######
+
 # define useful paths
 path = 'Exercise_geometric_transform/'
 set1_path = path + 'Photos_set_1/'
 set2_path = path + 'Photos_set_2/'
 res_path = path + 'Results/'
 
-# read input images and convert it into grey scale images
-img1 = cv.imread(set1_path + 'horizontal_center.jpg')
-img2 = cv.imread(set1_path + 'horizontal_right.jpg')
-gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+# 2 images with similarities
+img1_path = set1_path + 'horizontal_center.jpg'
+img2_path = set1_path + 'horizontal_right.jpg'
 
-# initiate ORB detector
-orb = cv.ORB_create()
-
-# kp --> Keypoints : array containing the feature points of the grayscale image
-# des --> Descriptor : array of size [nb keypoints]x128 containing informations about feature points
-
-# find the keypoints and compute the descriptors with ORB
-kp1 = orb.detect(gray1, None)
-kp2 = orb.detect(gray2, None)
-kp1, des1 = orb.compute(gray1, kp1)
-kp2, des2 = orb.compute(gray2, kp2)
-
-print('nb keypoints img1 : ' + len(kp1))
-
-# draw keypoints and save the result image
-img1_kp = gray1
-img1_kp = cv.drawKeypoints(gray1, kp1, img1_kp)
-cv.imwrite(res_path + 'orb_keypoints.jpg', img1_kp)
-
-# match keypoints between both images using brute-force
-bf = cv.BFMatcher()
-matches = bf.knnMatch(des1, des2, k=2)
-
-# apply ratio test to select only the strongest matches
+# ratio applied to selcet the strongest matches
 ratio = 0.7
-good_matches = []
-for m,n in matches:
-    if m.distance < ratio * n.distance:
-        good_matches.append([m])
 
-# draw matches and save the result image
-img_matches = cv.drawMatchesKnn(gray1, kp1, gray2, kp2, good_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-cv.imwrite(res_path + 'orb_matches.jpg', img_matches)
+gray1, gray2 = read_and_grey_images(img1_path, img2_path)
 
-# construct an array of pair of matched points
-matched_points = []
-for m in good_matches:
-    pt1 = kp1[m[0].queryIdx].pt
-    pt2 = kp2[m[0].trainIdx].pt
-    matched_points.append([pt1, pt2])
+matched_points = match_keypoints(gray1, gray2, ratio)
+
+M = compute_translation_matrix(matched_points)
 
 
-####### Translation model #######
 
-# Jacobian matrix
-J = np.array([[1, 0], 
-              [0, 1]])
-Jt = J.transpose()
 
-A = [[0, 0], 
-     [0, 0]]
-b = [[0],
-     [0]]
-for m in matched_points:
-    # delta = [[x0 - x1], [y0 - y1]]
-    delta = [[m[0][0] - m[1][0]], [m[0][1] - m[1][1]]]
-    A = A + Jt.dot(J)
-    b = b + Jt.dot(delta)
 
-p = solve(A, b)
 
-print(p)
-
-M = np.array([[1, 0, p[0][0]],
-              [0, 1, p[1][0]],
-              [0, 0, 0      ]])
 
 ###### Stitching ######
 
